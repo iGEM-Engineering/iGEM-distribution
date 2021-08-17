d95dfdd2acb98aaf3d6a0263a9352f3d1e38bdcb
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-08-17 12:48:17 -0400

    Convert RDF/XML to Sorted N-Triples

--- scripts/export_sbol3.py
+++ scripts/export_sbol3.py
@@ -2,23 +2,28 @@ import os
 import subprocess
 import sys
 
+import sbol3
+
 import scriptutils
 
 
 SBOL_CONVERTER = 'scripts/sbol'
 SBOL2_RDF_XML = 'iGEM_SBOL2_imports.xml'
-SBOL3_RDF_XML = 'iGEM_SBOL3_imports.xml'
 SBOL3_SORTED_NT = 'iGEM_SBOL3_imports.nt'
 
 
 def convert2to3(pkg_dir):
     sbol2_path = os.path.join(pkg_dir, SBOL2_RDF_XML)
-    sbol3_path = os.path.join(pkg_dir, SBOL3_RDF_XML)
+    sbol3_path = os.path.join(pkg_dir, SBOL3_SORTED_NT)
     cmd = [SBOL_CONVERTER, '-output', 'sbol3',
            'import', sbol2_path,
            'convert', '--target-sbol-version', '3']
-    with open(sbol3_path, 'w') as fp:
-        result = subprocess.check_call(cmd, stdout=fp)
+    # This will raise and exception if the command fails
+    proc = subprocess.run(cmd, capture_output=True, check=True)
+    # Now convert the RDF-XML to Sorted N-Triples
+    doc = sbol3.Document()
+    doc.read_string(proc.stdout.decode('utf-8'), sbol3.RDF_XML)
+    doc.write(sbol3_path, sbol3.SORTED_NT)
     return sbol3_path
 
 
