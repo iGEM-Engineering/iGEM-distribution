5ab85caf24d7e720421a31f5aa7f6de41e3092b4
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-08-19 16:39:34 -0400

    Refactor SBOL 2to3 conversion

--- scripts/export_sbol3.py
+++ scripts/export_sbol3.py
@@ -1,12 +1,10 @@
 import os
-import subprocess
 
 import sbol3
 
 import scriptutils
 
 
-SBOL_CONVERTER = 'scripts/sbol'
 SBOL2_RDF_XML = 'iGEM_SBOL2_imports.xml'
 SBOL3_SORTED_NT = 'iGEM_SBOL3_imports.nt'
 
@@ -14,19 +12,7 @@ SBOL3_SORTED_NT = 'iGEM_SBOL3_imports.nt'
 def convert2to3(pkg_dir):
     sbol2_path = os.path.join(pkg_dir, SBOL2_RDF_XML)
     sbol3_path = os.path.join(pkg_dir, SBOL3_SORTED_NT)
-    cmd = [SBOL_CONVERTER, '-output', 'sbol3',
-           'import', sbol2_path,
-           'convert', '--target-sbol-version', '3']
-    # This will raise an exception if the command fails
-    proc = subprocess.run(cmd, capture_output=True, check=True)
-    # Extract the rdf_xml output from the sbol converter
-    rdf_xml = proc.stdout.decode('utf-8')
-    # Post-process the conversion by updating object identities
-    rdf_xml = scriptutils.convert_identities2to3(rdf_xml)
-    doc = sbol3.Document()
-    doc.read_string(rdf_xml, sbol3.RDF_XML)
-    # Now convert the RDF-XML to Sorted N-Triples
-    doc.write(sbol3_path, sbol3.SORTED_NTRIPLES)
+    scriptutils.convert2to3(sbol2_path, sbol3_path, sbol3.SORTED_NTRIPLES)
     return sbol3_path
 
 
--- scripts/scriptutils/sbol2to3.py
+++ scripts/scriptutils/sbol2to3.py
@@ -1,5 +1,9 @@
+import subprocess
+
 import rdflib
 import sbol3
+
+SBOL_CONVERTER = 'scripts/sbol'
 
 
 def convert_identities2to3(sbol3_data: str) -> str:
@@ -20,19 +24,34 @@ def convert_identities2to3(sbol3_data: str) -> str:
             if o.startswith(sbol3.SBOL3_NS):
                 sbol3_type_count += 1
         if sbol3_type_count < 1:
+            # Not an SBOL object, so don't rename it
             continue
-        elif sbol3_type_count > 1:
-            # Isn't this an error?
-            print('SBOL3 type count is %d (more than one) for %r'
-                  % (sbol3_type_count, old_identity))
-            continue
-        # Update all triples where s is the subject
+        # Form the new identity from the original by lopping off the
+        # '/1' suffix
         new_identity = rdflib.URIRef(old_identity[:-2])
-        for s2, p2, o2 in g.triples((old_identity, None, None)):
-            g.add((new_identity, p2, o2))
-            g.remove((s2, p2, o2))
-        # Update all triples where s is the object
+        # Update all triples where old_identity is the subject
+        for s, p, o in g.triples((old_identity, None, None)):
+            g.add((new_identity, p, o))
+            g.remove((s, p, o))
+        # Update all triples where old_identity is the object
         for s, p, o in g.triples((None, None, old_identity)):
             g.add((s, p, new_identity))
             g.remove((s, p, o))
     return g.serialize(format="xml")
+
+
+def convert2to3(sbol2_path: str, sbol3_path: str, sbol3_format: str) -> str:
+    cmd = [SBOL_CONVERTER, '-output', 'sbol3',
+           'import', sbol2_path,
+           'convert', '--target-sbol-version', '3']
+    # This will raise an exception if the command fails
+    proc = subprocess.run(cmd, capture_output=True, check=True)
+    # Extract the rdf_xml output from the sbol converter
+    rdf_xml = proc.stdout.decode('utf-8')
+    # Post-process the conversion by updating object identities
+    rdf_xml = convert_identities2to3(rdf_xml)
+    doc = sbol3.Document()
+    doc.read_string(rdf_xml, sbol3.RDF_XML)
+    # Now convert the RDF-XML to Sorted N-Triples
+    doc.write(sbol3_path, sbol3_format)
+    return sbol3_path
