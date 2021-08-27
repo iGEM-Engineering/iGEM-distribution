35958bbe6bb1347579705e91646d2ce0470dd397
Jake Beal <jakebeal@gmail.com>
2021-08-27 09:57:57 -0500

    Add "local sequence file" option for data source

--- scripts/test/test_import.py
+++ scripts/test/test_import.py
@@ -22,6 +22,7 @@ def copy_to_tmp() -> str:
     testdir = os.path.dirname(os.path.realpath(__file__))
     print(f'test file is {__file__}')
     copy(os.path.join(testdir, 'testfiles', 'test_sequence.fasta'), tmpsub)
+    copy(os.path.join(testdir, 'testfiles', 'J23102-modified.fasta'), tmpsub)
     copy(os.path.join(testdir, 'testfiles', 'two_sequences.gb'), tmpsub)
     copy(os.path.join(testdir, 'testfiles', 'BBa_J23101.xml'), tmpsub)
     copy(os.path.join(testdir, 'testfiles', scripts.scriptutils.EXPORT_DIRECTORY, 'package_specification.nt'), tmpexport)
@@ -35,14 +36,16 @@ class TestImportParts(unittest.TestCase):
         inventory = part_retrieval.package_parts_inventory(tmpsub)
         unique_parts = set(inventory.values())
         print(f'Unique parts found: {unique_parts}')
-        assert len(unique_parts) == 4, f'Expected 4 parts, but found {len(unique_parts)}: {unique_parts}'
-        assert len(inventory) == 7, f'Expected 7 keys, but found {len(inventory)}: {inventory}'
+        assert len(unique_parts) == 5, f'Expected 5 parts, but found {len(unique_parts)}: {unique_parts}'
+        assert len(inventory) == 8, f'Expected 8 keys, but found {len(inventory)}: {inventory}'
         pkg = 'https://github.com/iGEM-Engineering/iGEM-distribution/test_package/'
         expected = {f'{pkg}NM_005341_4': f'{pkg}NM_005341_4',
                     f'{pkg}NM_005342': f'{pkg}NM_005342_4',
                     f'{pkg}NM_005342_4': f'{pkg}NM_005342_4',
                     f'{pkg}NM_005343': f'{pkg}NM_005343_4',
                     f'{pkg}NM_005343_4': f'{pkg}NM_005343_4',
+                    'https://github.com/iGEM-Engineering/iGEM-distribution/test_package/J23102-modified':
+                        'https://github.com/iGEM-Engineering/iGEM-distribution/test_package/J23102-modified',
                     'http://parts.igem.org/J23101':'http://parts.igem.org/J23101',
                     'http://parts.igem.org/J23101/1': 'http://parts.igem.org/J23101'}
         assert inventory == expected, f'Inventory does not match expected value: {inventory}'
