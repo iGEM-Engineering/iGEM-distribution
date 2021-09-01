c0facde59ad1c0dd2c77647d5de7bb84d1811a96
Jake Beal <jakebeal@gmail.com>
2021-08-31 21:45:21 -0500

    add summary for distribution contents

--- scripts/build_distribution.py
+++ scripts/build_distribution.py
@@ -1,5 +1,13 @@
 import scriptutils
 
+# find the root and packages
 root = scriptutils.distribution_dir()
 packages = scriptutils.package_dirs()
-scriptutils.build_distribution(root, packages)
+
+# build the distribution
+doc = scriptutils.build_distribution(root, packages)
+
+# make the summary Markdown
+scriptutils.generate_distribution_summary(root, doc)
+
+# export materials in GenBank and FASTA for independent inspection and synthesis
