6035b604171d054fa055f8b3e8be349930cfc17b
Jake Beal <jakebeal@gmail.com>
2021-09-11 21:32:49 -0500

    use sbol-utilities plasmid function instead of roll-out-own one that is crashing

--- scripts/scriptutils/helpers.py
+++ scripts/scriptutils/helpers.py
@@ -1,15 +1,5 @@
 import sbol3
-import tyto
-
-
-def has_vector_role(component: sbol3.Component) -> bool:
-    """Check if a Component has a vector role (i.e., is a plasmid or similar)
-
-    :param component: SBOL3 component to check
-    :return: true if the component is a vector
-    """
-    return any(r for r in component.roles
-               if tyto.SO.plasmid.is_ancestor_of(r) or tyto.SO.vector_replicon.is_ancestor_of(r))
+from sbol_utilities.helper_functions import is_plasmid
 
 
 def vector_to_insert(component: sbol3.Component) -> sbol3.Component:
@@ -21,9 +11,8 @@ def vector_to_insert(component: sbol3.Component) -> sbol3.Component:
     :return: component if not vector; otherwise the vector
     """
     # is either the component or any feature thereof a vector? If not, then return component
-    subvectors = {f for f in component.features
-                  if isinstance(f, sbol3.SubComponent) and has_vector_role(f.instance_of.lookup()) }
-    if len(subvectors) == 0 and not has_vector_role(component):
+    subvectors = {f for f in component.features if is_plasmid(f.instance_of.lookup())}
+    if len(subvectors) == 0 and not is_plasmid(component):
         return component
     # otherwise, if there's precisely one non-vector subcomponent, return the Component it points to
     inserts = {f for f in set(component.features)-subvectors if isinstance(f, sbol3.SubComponent)}
