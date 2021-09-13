3ac91e37f4ea266671e18585e58cfcddb5906ca2
Jake Beal <jakebeal@gmail.com>
2021-09-12 19:50:20 -0500

    Add vector information to package summaries (resolves #80) Also categorize parts via role information

--- scripts/scriptutils/generate_markdown.py
+++ scripts/scriptutils/generate_markdown.py
@@ -14,9 +14,11 @@ SUMMARY_FILE = 'README.md'
 DISTRIBUTION_SUMMARY = 'README_distribution.md'
 """File name for the distribution summary, to be located in the root directory"""
 
+
 def hilite(text: str):
     """Emphasize a piece of markdown text with bold and red formatting"""
-    return f' **<span style="color:red">{text}</span>**'
+    return f' _<span style="color:red">{text}</span>_'
+
 
 def generate_package_summary(package: str, doc: sbol3.Document):
     """Generate a Markdown README file summarizing package contents that is suitable for automatic display on GitHub
@@ -25,7 +27,6 @@ def generate_package_summary(package: str, doc: sbol3.Document):
     :param doc: package document to summarize
     :return: None
     """
-    # TODO: use combinatorial derivations and expansions
     # Find the key elements
     parts_list = doc.find(sbol_utilities.excel_to_sbol.BASIC_PARTS_COLLECTION)
     build_plan = doc.find(BUILD_PRODUCTS_COLLECTION)
@@ -34,31 +35,56 @@ def generate_package_summary(package: str, doc: sbol3.Document):
     if not isinstance(build_plan, sbol3.Collection):
         raise ValueError(f'Could not find build plan in package {package}')
 
-    # compute all required statistics
+    # compute all desired statistics
     parts_used = contained_components(build_plan)
     ids_of_parts_used = {c.identity for c in parts_used}
     unused_parts = {str(m) for m in parts_list.members} - ids_of_parts_used
 
+    vector_parts = [m.lookup() for m in parts_list.members if is_plasmid(m.lookup())]
+    non_vector_parts = [m.lookup() for m in parts_list.members if m.lookup() not in vector_parts]
+    missing_seq = {m.identity for m in non_vector_parts if len(m.sequences) == 0}
+    missing_vec = {m.display_id for m in vector_parts if len(m.sequences) == 0}
+
+    # inventory parts by sequence ontology category, e.g., promoter, CDS, terminator
+    so_roles = {}
+    so_clusters = {}
+    unknown_role_count = 0
+    for c in non_vector_parts:
+        so_roles[c.identity] = [tyto.SO.get_term_by_uri(t) for t in c.roles if has_SO_uri(t)]
+        if not so_roles[c.identity]:
+            unknown_role_count += 1
+        for r in so_roles[c.identity]:
+            so_clusters[r] = so_clusters.get(r, set()).union({c.identity})
+
+    # inventory what inserts are in what vectors
+    insert_vectors = {}
+    for m in build_plan.members:
+        c = m.lookup()
+        parts = contained_components(c)
+        in_vector = parts.intersection(vector_parts)
+        for p in (parts - in_vector):
+            insert_vectors[p] = insert_vectors.get(p, set()).union({v.display_id for v in in_vector})
+
     # write the README file
-    summary_filename = os.path.join(package,SUMMARY_FILE)
-    with open(summary_filename,'w') as f:
+    summary_filename = os.path.join(package, SUMMARY_FILE)
+    with open(summary_filename, 'w') as f:
         # First the package name and description
         f.write(f'# Package: {parts_list.name}\n\n')
         f.write(f'{parts_list.description}\n\n')
 
-        # Next, statistics and errors
+        # Next, summary statistics (with accompanying errors)
         f.write(f'### Summary:\n\n')
         # Part count
-        # TODO: separate parts from vectors
-        vectors = [m for m in parts_list.members if is_plasmid(m.lookup())]
-        non_vectors = [m for m in parts_list.members if m not in vectors]
-        missing_seq = [str(m) for m in non_vectors if len(m.lookup().sequences) == 0]
-        missing_vec = [str(m) for m in vectors if len(m.lookup().sequences) == 0]
-        missings = f' **<span style="color:red">{len(missing_seq)} missing sequences</span>**' if missing_seq else ''
-        f.write(f'- {len(non_vectors)} parts{missings}\n')
-        missings = f' **<span style="color:red">{len(missing_vec)} missing sequences</span>**' if missing_vec else ''
-        f.write(f'- {len(vectors)} vectors{missings}\n')
-        # TODO: inventory the common types of parts, e.g., promoter, CDS, terminator
+        missed = hilite(f'{len(missing_seq)} missing sequences') if missing_seq else ''
+        f.write(f'- {len(non_vector_parts)} parts{missed}\n')
+        for role in sorted(so_clusters):
+            f.write(f'    - {role}: {len(so_clusters[role])}\n')
+        f.write(f'    - unspecified role: {unknown_role_count}\n')
+        if missing_vec:
+            missed = hilite(f'{len(missing_vec)} missing sequences: {", ".join(sorted(missing_vec))}')
+        else:
+            missed = ''
+        f.write(f'- {len(vector_parts)} vectors{missed}\n')
         # Build information
         f.write(f'- {len(build_plan.members)} samples for distribution')
         if unused_parts:
@@ -69,16 +95,17 @@ def generate_package_summary(package: str, doc: sbol3.Document):
 
         # Finally, a list of all the parts and their UIDs
         f.write(f'### Parts:\n\n')
-        for p in id_sort(m.lookup() for m in parts_list.members):
+        for p in id_sort(non_vector_parts):
+            # id / name
             f.write(f'- {p.display_id}')
             if p.name and sbol_utilities.excel_to_sbol.string_to_display_id(p.name) != p.display_id:
                 f.write(f': {p.name}')
-            # TODO: more principled handling of role lookup
-            #SO_roles = []
-            SO_roles = [tyto.SO.get_term_by_uri(t) for t in p.roles if has_SO_uri(t)]
-            if SO_roles:
-                f.write(f' ({", ".join(SO_roles)})')
-            if p.identity in missing_seq or p.identity in missing_vec:
+            # roles
+            if so_roles.get(p.identity, None):
+                f.write(f' ({", ".join(sorted(so_roles[p.identity]))})')
+            if p in insert_vectors:
+                f.write(f' in vectors {", ".join(sorted(insert_vectors[p]))}')
+            if p.identity in missing_seq:
                 f.write(hilite(f'missing sequence'))
             if p.identity in unused_parts:
                 f.write(hilite(f'not included in distribution'))
--- scripts/test/test_files/README.md
+++ scripts/test/test_files/README.md
@@ -5,19 +5,21 @@ The Anderson Promoters are a well-established set of constitutive promoters at v
 ### Summary:
 
 - 7 parts
-- 2 vectors **<span style="color:red">1 missing sequences</span>**
-- 10 samples for distribution **<span style="color:red">1 parts not included</span>**
+    - CDS: 1
+    - engineered_region: 1
+    - promoter: 3
+    - unspecified role: 2
+- 2 vectors _<span style="color:red">1 missing sequences: pOpen_v4</span>_
+- 10 samples for distribution _<span style="color:red">1 parts not included</span>_
 
 ### Parts:
 
-- BBa_J364007 **<span style="color:red">not included in distribution</span>**
-- J23102_modified
-- pOpen_v4 (plasmid) **<span style="color:red">missing sequence</span>**
-- BBa_J23100 (promoter)
-- BBa_J23101 (promoter)
-- BBa_J23102 (promoter)
-- pSB1C3 (plasmid_vector)
-- LmrA (CDS)
-- JWYZ01000115 (engineered_region)
+- BBa_J364007 _<span style="color:red">not included in distribution</span>_
+- J23102_modified in vectors pOpen_v4, pSB1C3
+- BBa_J23100 (promoter) in vectors pOpen_v4, pSB1C3
+- BBa_J23101 (promoter) in vectors pOpen_v4, pSB1C3
+- BBa_J23102 (promoter) in vectors pOpen_v4, pSB1C3
+- LmrA (CDS) in vectors pSB1C3
+- JWYZ01000115 (engineered_region) in vectors pSB1C3
 
 _Note: automatically generated from package Excel and sequence files; do not edit_
