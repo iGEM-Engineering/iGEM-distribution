073d103384ffd6bf6f91ae753e3c40d2d62a2bd6
GC-repeat <63684428+GC-repeat@users.noreply.github.com>
2022-02-21 20:47:01 -0500

    Correct PyLint errors for regularize_directories.py

--- scripts/regularize_directories.py
+++ scripts/regularize_directories.py
@@ -1,8 +1,10 @@
+''' Check packages structure '''
+
 import os
 import sys
 import scriptutils
 
-error = False
+ERROR = False
 packages = scriptutils.package_dirs()
 print(f'Scanning; found {len(packages)} packages')
 for p in packages:
@@ -11,8 +13,8 @@ for p in packages:
         scriptutils.regularize_directory(p)
     except ValueError as e:
         print(f'Bad structure for package {os.path.basename(p)}: {e}')
-        error = True
+        ERROR = True
 
 # If there was an error, flag on exit in order to notify executing YAML script
-if error:
+if ERROR:
     sys.exit(1)
