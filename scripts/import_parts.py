b53e695cd007c89c4e2d787d0eb044ebb445aa5a
GC-repeat <63684428+GC-repeat@users.noreply.github.com>
2022-02-21 19:52:31 -0500

    Correct PyLint errors for import_parts.py

--- scripts/import_parts.py
+++ scripts/import_parts.py
@@ -1,13 +1,15 @@
+''' Import parts for packages '''
+
 import os
 import sys
 import scriptutils
 
-error = False
+ERROR = False
 packages = scriptutils.package_dirs()
 for p in packages:
     print(f'Importing parts for package {os.path.basename(p)}')
     scriptutils.import_parts(p)
 
 # If there was an error, flag on exit in order to notify executing YAML script
-if error:
+if ERROR:
     sys.exit(1)
