10e117ed15c9d5c523363867b3a38242c8db31fd
GC-repeat <63684428+GC-repeat@users.noreply.github.com>
2022-02-21 19:50:10 -0500

    Correct PyLint errors for expor_csvs.py

--- scripts/export_csvs.py
+++ scripts/export_csvs.py
@@ -1,8 +1,10 @@
+''' Export CSV files for packages '''
+
 import os
 import sys
 import scriptutils
 
-error = False
+ERROR = False
 packages = scriptutils.package_dirs()
 for p in packages:
     print(f'Exporting CSVs for package {os.path.basename(p)}')
@@ -10,8 +12,8 @@ for p in packages:
         scriptutils.export_csvs(p)
     except OSError as e:
         print(f'Could not export CSV files for package {os.path.basename(p)}: {e}')
-        error = True
+        ERROR = True
 
 # If there was an error, flag on exit in order to notify executing YAML script
-if error:
+if ERROR:
     sys.exit(1)
