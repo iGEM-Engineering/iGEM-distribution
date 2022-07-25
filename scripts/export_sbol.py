7ddc2dc6f6af6e47732ba382f24820d627fa7772
GC-repeat <63684428+GC-repeat@users.noreply.github.com>
2022-07-25 15:31:47 -0400

    Revert "Correct PyLint errors for export_sbol.py"
    This reverts commit 4c63d2beea9ba492b69277a08b7d66b4319d96bc.

--- scripts/export_sbol.py
+++ scripts/export_sbol.py
@@ -1,10 +1,8 @@
-''' Export SBOL files for packages '''
-
 import os
 import sys
 import scriptutils
 
-ERROR = False
+error = False
 packages = scriptutils.package_dirs()
 for p in packages:
     print(f'Exporting SBOL from Excel for package {os.path.basename(p)}')
@@ -13,8 +11,8 @@ for p in packages:
         print(f'  {len(doc.objects)} designs and collections exported')
     except (OSError, ValueError) as e:
         print(f'Could not export SBOL file for package {os.path.basename(p)}: {e}')
-        ERROR = True
+        error = True
 
 # If there was an error, flag on exit in order to notify executing YAML script
-if ERROR:
+if error:
     sys.exit(1)
