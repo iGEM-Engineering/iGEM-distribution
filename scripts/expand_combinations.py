92c70b412093c50ee8371a574218b5d5bbd4c129
GC-repeat <63684428+GC-repeat@users.noreply.github.com>
2022-07-25 15:32:15 -0400

    Revert "Correct PyLint errors for expand_combinations.py"
    This reverts commit 1edca23deaab5c522061f4b3a599bd58d33d9a27.

--- scripts/expand_combinations.py
+++ scripts/expand_combinations.py
@@ -1,11 +1,9 @@
-''' Compute build plan for packages '''
-
 import os
 import sys
 
 import scriptutils
 
-ERROR = False
+error = False
 packages = scriptutils.package_dirs()
 for p in packages:
 
@@ -15,8 +13,8 @@ for p in packages:
 
     except (OSError, ValueError) as e:
         print(f'Could not compute build plan for {os.path.basename(p)}: {e}')
-        ERROR = True
+        error = True
 
 # If there was an error, flag on exit in order to notify executing YAML script
-if ERROR:
+if error:
     sys.exit(1)
