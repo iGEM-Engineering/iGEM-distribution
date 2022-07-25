c4e22452c410a076223d0b25f9b4c3bdf7f23715
GC-repeat <63684428+GC-repeat@users.noreply.github.com>
2022-07-25 15:35:05 -0400

    Revert "Correct PyLint errors for expor_csvs.py"
    This reverts commit 10e117ed15c9d5c523363867b3a38242c8db31fd.

--- scripts/export_csvs.py
+++ scripts/export_csvs.py
@@ -1,10 +1,8 @@
-''' Export CSV files for packages '''
-
 import os
 import sys
 import scriptutils
 
-ERROR = False
+error = False
 packages = scriptutils.package_dirs()
 for p in packages:
     print(f'Exporting CSVs for package {os.path.basename(p)}')
@@ -12,8 +10,8 @@ for p in packages:
         scriptutils.export_csvs(p)
     except OSError as e:
         print(f'Could not export CSV files for package {os.path.basename(p)}: {e}')
-        ERROR = True
+        error = True
 
 # If there was an error, flag on exit in order to notify executing YAML script
-if ERROR:
+if error:
     sys.exit(1)
