2d89a32ce872dba54220804680cc93fa7d1e9cc6
GC-repeat <63684428+GC-repeat@users.noreply.github.com>
2022-07-25 15:34:31 -0400

    Revert "Correct PyLint errors for convert_sbol_2to3.py"
    This reverts commit 0d4ba997b8be9fe8e5d2852bc3d3644225b9cf43.

--- scripts/convert_sbol_2to3.py
+++ scripts/convert_sbol_2to3.py
@@ -1,5 +1,3 @@
-''' Convert SBOL2 to SBOL3 files for packages '''
-
 import os
 import sys
 import git
@@ -8,11 +6,11 @@ import scriptutils
 
 # find the repository for automatic git actions, if possible
 try:
-REPO = git.Repo('.', search_parent_directories=True)
+    repo = git.Repo('.', search_parent_directories=True)
 except git.InvalidGitRepositoryError:
-    REPO = None
+    repo = None
 
-ERROR = False
+error = False
 packages = scriptutils.package_dirs()
 for p in packages:
 
@@ -21,14 +19,14 @@ for p in packages:
         # convert files
         mappings = scriptutils.convert_package_sbol2_files(p)
         # if there's a git repo, try to remove the old files
-        if REPO and len(mappings):
-            REPO.index.add(mappings.keys())     # add, in case they weren't there before
-            REPO.index.remove(mappings.keys(), working_tree=True, f=True)  # then remove
+        if repo and len(mappings):
+            repo.index.add(mappings.keys())     # add, in case they weren't there before
+            repo.index.remove(mappings.keys(), working_tree=True, f=True)  # then remove
 
     except (OSError, ValueError) as e:
         print(f'Could not convert SBOL2 files for package {os.path.basename(p)}: {e}')
-        ERROR = True
+        error = True
 
 # If there was an error, flag on exit in order to notify executing YAML script
-if ERROR:
+if error:
     sys.exit(1)
