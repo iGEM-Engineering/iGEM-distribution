d7ea8883ae1a2ea52804d9db5807ea4f036d2e79
GC-repeat <63684428+GC-repeat@users.noreply.github.com>
2022-07-25 15:34:48 -0400

    Revert "Correct PyLint errors for import_parts.py"
    This reverts commit b53e695cd007c89c4e2d787d0eb044ebb445aa5a.

--- scripts/import_parts.py
+++ scripts/import_parts.py
@@ -1,15 +1,13 @@
-''' Import parts for packages '''
-
 import os
 import sys
 import scriptutils
 
-ERROR = False
+error = False
 packages = scriptutils.package_dirs()
 for p in packages:
     print(f'Importing parts for package {os.path.basename(p)}')
     scriptutils.import_parts(p)
 
 # If there was an error, flag on exit in order to notify executing YAML script
-if ERROR:
+if error:
     sys.exit(1)
